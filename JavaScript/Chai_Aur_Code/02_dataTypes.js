1c76751bc184c70778dac5bfd3ffdf42b7de544b
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-05-07 22:42:26 +0600

    Create 02_dataTypes.js

---
+++ JavaScript/Chai_Aur_Code/02_dataTypes.js
@@ -0,0 +1,39 @@
+"user strict";  //Treat all js code as newer version
+// It will show error if we use undeclared variable
+
+let name = "Stark";
+let age = 22;
+let isLoggedIn = false;
+let state;
+/*
+Data types
+1. Primitive Data Types
+1.1. String => ""
+1.2. Number => 1, 1.5 
+            * ?Range   -2^53 to 2^53
+1.3. Boolean => true, false
+1.4. Undefined => undefined
+1.5. Null => null
+1.6. Symbol => Symbol()
+1.7. BigInt => 9007199254740991n
+
+
+2. Reference Data Types
+
+2.1. Array => []
+2.2. Object => {}
+2.3. Function => function(){}
+2.4. Date => new Date()
+
+*/
+
+/*
+* ? typeof used to check the data type of a variable
+*/
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+
+
